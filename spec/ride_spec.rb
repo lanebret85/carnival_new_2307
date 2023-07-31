bf2d25bc0d504dc2e4cef934fdbd84e8e5c48dc7
Lane Bretschneider <lanebret85@gmail.com>
2023-07-31 09:10:15 -0700

    refactor: modify tests for cleaner organization of board_rider method tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -19,46 +19,14 @@ RSpec.describe Ride do
     end
   end
 
-  describe "#total_revenue" do
-    it "returns 0 to start" do
+  describe "#board_rider" do
+    it "returns total revenue of 0 to start" do
       ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
 
       expect(ride1.total_revenue).to eq(0)
     end
 
-    it "can add the admission fee to its total revenue with each rider and return the total revenue" do
-      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
-
-      visitor1 = Visitor.new('Bruce', 54, '$10')
-      visitor2 = Visitor.new('Tucker', 36, '$5')
-
-      visitor1.add_preference(:gentle)
-      visitor2.add_preference(:gentle)
-
-      ride1.board_rider(visitor1)
-      ride1.board_rider(visitor2)
-      ride1.board_rider(visitor1)
-
-      expect(ride1.total_revenue).to eq(3)
-
-      visitor3 = Visitor.new('Penny', 64, '$15')
-
-      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
-      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
-
-      visitor2.add_preference(:thrilling)
-      visitor3.add_preference(:thrilling)
-
-      ride3.board_rider(visitor1)
-      ride3.board_rider(visitor2)
-      ride3.board_rider(visitor3)
-
-      expect(ride3.total_revenue).to eq(2)
-    end
-  end
-
-  describe "#rider_log" do
-    it "can return a log of how many times each rider has ridden a ride" do
+    it "can return a rider log of how many times each rider has ridden a ride, subtract the admission fee from each rider's spending money with each ride, and add the admission fee to its total revenue with each rider and return the total revenue" do
       ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
 
       visitor1 = Visitor.new('Bruce', 54, '$10')
@@ -72,37 +40,9 @@ RSpec.describe Ride do
       ride1.board_rider(visitor1)
 
       expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
-
-      visitor3 = Visitor.new('Penny', 64, '$15')
-
-      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
-      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
-
-      visitor2.add_preference(:thrilling)
-      visitor3.add_preference(:thrilling)
-
-      ride3.board_rider(visitor1)
-      ride3.board_rider(visitor2)
-      ride3.board_rider(visitor3)
-
-      expect(ride3.rider_log).to eq({visitor3 => 1})
-    end
-    
-    it "can subtract the admission fee from a rider's spending money with each ride" do
-      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
-  
-      visitor1 = Visitor.new('Bruce', 54, '$10')
-      visitor2 = Visitor.new('Tucker', 36, '$5')
-  
-      visitor1.add_preference(:gentle)
-      visitor2.add_preference(:gentle)
-  
-      ride1.board_rider(visitor1)
-      ride1.board_rider(visitor2)
-      ride1.board_rider(visitor1)
-  
       expect(visitor1.spending_money).to eq(8)
       expect(visitor2.spending_money).to eq(4)
+      expect(ride1.total_revenue).to eq(3)
 
       visitor3 = Visitor.new('Penny', 64, '$15')
 
@@ -119,6 +59,8 @@ RSpec.describe Ride do
       expect(visitor1.spending_money).to eq(8)
       expect(visitor2.spending_money).to eq(4)
       expect(visitor3.spending_money).to eq(13)
+      expect(ride3.rider_log).to eq({visitor3 => 1})
+      expect(ride3.total_revenue).to eq(2)
     end
   end
 end
