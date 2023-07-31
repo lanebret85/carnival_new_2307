6068080885f072cdf44dd82f9ad81d84bddf1344
Lane Bretschneider <lanebret85@gmail.com>
2023-07-31 09:44:09 -0700

    feat: add board_rider method to Ride class and add attr_accessor for spending_money attribute in Visitor class

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,12 +2,26 @@ class Ride
   attr_reader :name,
               :min_height,
               :admission_fee,
-              :excitement
+              :excitement,
+              :total_revenue,
+              :rider_log
 
   def initialize(ride_details)
     @name = ride_details[:name]
     @min_height = ride_details[:min_height]
     @admission_fee = ride_details[:admission_fee]
     @excitement = ride_details[:excitement]
+    @total_revenue = 0
+    @rider_log = Hash.new(0)
+  end
+
+  def board_rider(rider)
+    if rider.tall_enough?(@min_height) &&
+    rider.preferences.include?(@excitement) &&
+    rider.spending_money >= @admission_fee
+      rider_log[rider] += 1
+      rider.spending_money -= @admission_fee
+      @total_revenue += @admission_fee
+    end
   end
 end
